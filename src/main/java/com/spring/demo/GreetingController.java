7fc08b2a96bcd14c53cbd39b237b92b2018c184d
rwhite53 <rwhite@october-design.com>
2025-02-27 19:55:53 -0500

    WIP: small formatting and code clean up

--- src/main/java/com/spring/demo/GreetingController.java
+++ src/main/java/com/spring/demo/GreetingController.java
@@ -1,6 +1,6 @@
 package com.spring.demo;
 
-import org.springframework.context.annotation.Bean;
+// import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -24,25 +24,24 @@ public class GreetingController implements WebMvcConfigurer {
     // adds `greetingForm` object as view model for this path
     @GetMapping("/greeting")
     public String greeting(
-        @RequestParam(name="name", required=false, defaultValue="Grand Vizier") String name, 
-        Model model,
-        GreetingForm greetingForm
-    ) {
+            @RequestParam(name = "name", required = false, defaultValue = "Grand Vizier") String name,
+            Model model,
+            GreetingForm greetingForm) {
         model.addAttribute("name", name);
         return "greeting";
     }
-
 
     // on submit validates input fields defined in bound GreetingForm
     // on submit passes input values to model GreetingResult instead of form
     // redirects view to result.html
     @PostMapping("/greeting")
     public String checkGreetingInfo(
-        GreetingForm greetingForm,
-        BindingResult bindingResult
-    ) {
+            GreetingForm greetingForm,
+            BindingResult bindingResult) {
         // handle invalid input here (not provided in demo)
-        if (bindingResult.hasErrors()) { return "form"; }
+        if (bindingResult.hasErrors()) {
+            return "form";
+        }
         String newname = bindingResult.getFieldValue("name").toString();
 
         System.out.println("post valid GreetingForm.name: " + newname);
@@ -54,10 +53,9 @@ public class GreetingController implements WebMvcConfigurer {
     // adds `greetingForm` object as view model for this path
     @GetMapping("/greeting/result")
     public String greetingResult(
-        @RequestParam(name="name", required=false, defaultValue="You Again?") String name,
-        @ModelAttribute GreetingForm greeting, 
-        Model model
-    ) {
+            @RequestParam(name = "name", required = false, defaultValue = "You Again?") String name,
+            @ModelAttribute GreetingForm greeting,
+            Model model) {
         model.addAttribute("greeting", greeting);
         return "result";
     }
